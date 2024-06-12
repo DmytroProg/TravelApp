ec63a501284f308ea4df06acd0b87511a53bee22
DmytroProg <dmytroprog@gmail.com>
2024-06-12 18:22:44 +0300

    added models User, Country, Supply

---
+++ TravelApp.Domain/Models/Supply.cs
@@ -0,0 +1,7 @@
+﻿namespace TravelApp.Domain.Models;
+
+public class Supply
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+}
---
+++ TravelApp.Domain/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelApp.Domain.Models;
+
+[Serializable]
+public class User
+{
+    public int Id { get; set; }
+    public string Nickname { get; set; }
+    public string EmailOrPhoneNumber { get; set; }
+    public string Password { get; set; }
+    public string IconPath { get; set; }
+}
