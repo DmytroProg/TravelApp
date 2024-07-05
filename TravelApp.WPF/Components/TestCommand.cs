8b082d69e9a239fff1693d6b99e50e1366328512
Snost <snizhana.lala@gmail.com>
2024-07-05 10:41:26 +0300

    done

---
+++ TravelApp.WPF/Components/TestCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace TravelApp;
+
+internal class TestCommand : ICommand
+{
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        MessageBox.Show("Test Command");
+    }
+}
---
+++ TravelApp.WPF/Components/UserInfoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace TravelApp.WPF
+{
+    internal class UserInfoViewModel
+    {
+        public ICommand SaveCommand { get; set; }
+        public string FullName { get; set; }
+        public string NumberOrEmail { get; set; }
+        public UserInfoViewModel() {
+            SaveCommand = new TestCommand();
+        }
+    }
+
+   
+
+}
